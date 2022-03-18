2833d1cc3c27474848ea9418bb3dba7e2aa3845d
Aliseiko <aliseiko.dev@gmail.com>
2022-03-18 23:09:53 +0300

    fix: close menu on link click

---
+++ shelter/assets/pages/main/index.js
@@ -0,0 +1,25 @@
+const burgerButton = document.querySelector('.burger'),
+    menuCover = document.querySelector('.menu-cover'),
+    nav = document.querySelector('.nav');
+
+// --------------- open / close menu ------------------
+function openCloseMenu() {
+  menuCover.classList.toggle('menu-cover_active');
+  burgerButton.classList.toggle('burger_active');
+  nav.classList.toggle('nav_active');
+}
+
+function closeMenu() {
+  menuCover.classList.remove('menu-cover_active');
+  burgerButton.classList.remove('burger_active');
+  nav.classList.remove('nav_active');
+}
+
+burgerButton.addEventListener('click', openCloseMenu);
+menuCover.addEventListener('click', openCloseMenu);
+nav.addEventListener('click', event => {
+  if (event.target.classList.contains('menu__link')) {
+    closeMenu()
+  }
+})
+
